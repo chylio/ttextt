c84cb2b23a35738bd29f39debd7394c6cfe1274c
chylio <chylio127@gmail.com>
2026-01-15 22:56:49 +0800

    Create initialDoctors.js

---
+++ src/data/initialDoctors.js
@@ -0,0 +1,9 @@
+export const INITIAL_DOCTORS = [
+  { id: 1, name: "李偉廷", level: "VS", dept: "OS", dailyCount: 3, targets: { os: 5, endo: 2, pros: 2 }, current: { os: 4, endo: 1, pros: 1 } },
+  { id: 2, name: "許育瑞", level: "Fixed R", dept: "Pros", dailyCount: 4, targets: { os: 2, endo: 2, pros: 5 }, current: { os: 1, endo: 1, pros: 4 } },
+  { id: 3, name: "官語晨", level: "PGY", dept: "Perio", dailyCount: 1, targets: { os: 5, endo: 5, pros: 5 }, current: { os: 1, endo: 4, pros: 1 } },
+  { id: 4, name: "廖怡清", level: "Intern", dept: "Endo", dailyCount: 2, targets: { os: 3, endo: 5, pros: 2 }, current: { os: 1, endo: 2, pros: 1 } },
+  { id: 5, name: "林靖傑", level: "Intern", dept: "家牙", dailyCount: 4, targets: { os: 2, endo: 2, pros: 5 }, current: { os: 1, endo: 1, pros: 3 } },
+  { id: 6, name: "李俊育", level: "PGY", dept: "Pedo", dailyCount: 0, targets: { os: 5, endo: 5, pros: 5 }, current: { os: 0, endo: 0, pros: 0 } },
+  { id: 7, name: "楊淳琇", level: "Fixed R", dept: "Ortho", dailyCount: 2, targets: { os: 2, endo: 2, pros: 2 }, current: { os: 1, endo: 1, pros: 1 } },
+];
